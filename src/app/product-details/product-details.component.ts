60dca6f87201d19bc617896e46904f40d2192c3c
Yaroslav Myuller <yaroslav.myuller@ista.com>
2023-01-30 18:30:49 +0100

    implement service cart in product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Product, products} from "../products";
 import {ActivatedRoute} from "@angular/router";
+import {CartService} from '../cart.service';
 
 @Component({
     selector: 'app-product-details',
@@ -12,7 +13,7 @@ export class ProductDetailsComponent {
     product: Product | undefined;
 
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private cartService: CartService) {
     }
 
     ngOnInit() {
@@ -20,5 +21,10 @@ export class ProductDetailsComponent {
         const productIdFromRoute = Number(routeParams.get('productId'));
 
         this.product = products.find(product => product.id === productIdFromRoute);
+    }
+
+    addToCart(product: Product) {
+        this.cartService.addToCart(product);
+        window.alert('Your product has been added to the cart!');
     }
 }
