fb04318b2483ee7b96a0d412054f832f26eaed8a
Yaroslav Myuller <yaroslav.myuller@ista.com>
2023-01-30 18:23:49 +0100

    implement first functionalities in service cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Product} from "./products";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+    items: Product[] = [];
+
+    addToCart(product: Product): void {
+        this.items.push(product);
+    }
+
+    getItems(): Product[] {
+        return this.items;
+    }
+
+    clearCart(): Product[] {
+        this.items = [];
+        return this.items;
+    }
 }
