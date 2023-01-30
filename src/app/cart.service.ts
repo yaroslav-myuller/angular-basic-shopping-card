f23e04dbf30e7b5fc68b87773af3138aecea826b
Yaroslav Myuller <yaroslav.myuller@ista.com>
2023-01-30 18:54:45 +0100

    implement get shipping prices function in cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Product} from "./products";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -7,6 +9,9 @@ import {Product} from "./products";
 export class CartService {
 
     items: Product[] = [];
+
+    constructor(private http: HttpClient) {
+    }
 
     addToCart(product: Product): void {
         this.items.push(product);
@@ -19,5 +24,9 @@ export class CartService {
     clearCart(): Product[] {
         this.items = [];
         return this.items;
+    }
+
+    getShippingPrices(): Observable<{ type: string, price: number }[]> {
+        return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
     }
 }
