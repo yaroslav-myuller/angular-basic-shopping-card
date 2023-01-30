8e7cd7906fe7d4bb8159a11cdc7685b7b93c4b6d
Yaroslav Myuller <yaroslav.myuller@ista.com>
2023-01-30 18:48:02 +0100

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import {TopBarComponent} from './top-bar/top-bar.component';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
+import {CartComponent} from './cart/cart.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
     imports: [
         BrowserModule,
+        HttpClientModule,
         ReactiveFormsModule,
         RouterModule.forRoot([
             {path: '', component: ProductListComponent},
