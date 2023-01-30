09f3f18f0b5b899e22c585a64af347fb421bfc84
Yaroslav Myuller <yaroslav.myuller@ista.com>
2023-01-30 18:14:59 +0100

    implement product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Product, products} from "../products";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+    selector: 'app-product-details',
+    templateUrl: './product-details.component.html',
+    styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
 
+    product: Product | undefined;
+
+
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        const routeParams = this.route.snapshot.paramMap;
+        const productIdFromRoute = Number(routeParams.get('productId'));
+
+        this.product = products.find(product => product.id === productIdFromRoute);
+    }
 }
